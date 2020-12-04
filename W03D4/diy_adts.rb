4817e274a871e1ec0694a4f04b51f114da1386e5
droid4alex <droid4alex@gmail.com>
2020-12-04 05:46:55 -0800

    add diy_adts.rb

---
+++ W03D4/diy_adts.rb
@@ -0,0 +1,57 @@
+class Stack
+    def initialize
+      # create ivar to store stack here!
+    end
+
+    def push(el)
+      # adds an element to the stack
+    end
+
+    def pop
+      # removes one element from the stack
+    end
+
+    def peek
+      # returns, but doesn't remove, the top element in the stack
+    end
+  end
+  
+class Queue
+    def initialize
+
+    end
+
+    def enqueue(el)
+
+    end
+
+    def dequeue
+
+    end
+
+    def peek
+      # returns, but doesn't remove, the top element in the stack
+    end
+  end
+  
+class Map
+    def initialize
+
+    end
+
+    def set(key, value)
+        # Note that the set method can be used to either create a new key-value pair or update the value for a pre-existing key. It's up to you to check whether a key currently exists!
+    end
+
+    def get(key)
+
+    end
+
+    def delete(key)
+
+    end
+
+    def show
+
+    end
+  end
