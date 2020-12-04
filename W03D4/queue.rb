6f5a578a067d3b456e5a433d857b9d5d8d341427
droid4alex <droid4alex@gmail.com>
2020-12-03 22:30:56 -0800

    add W03D4 stack, queue, map

---
+++ W03D4/queue.rb
@@ -0,0 +1,17 @@
+class Queue
+    def initialize
+
+    end
+
+    def enqueue(el)
+
+    end
+
+    def dequeue
+
+    end
+
+    def peek
+      # returns, but doesn't remove, the top element in the stack
+    end
+  end
